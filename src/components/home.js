de46c10e905db11048e5c5cd0b4cee661f0ef3b1
hector <elmejordx06@gmail.com>
2020-02-12 13:16:01 -0400

    importando modulo

--- src/components/home.js
+++ src/components/home.js
@@ -2,20 +2,14 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 
-//componets
-import Home from './components/home';
-
 class Prueba extends Component {
  render() {
 
    return (
-     <Home />
+     <h1> Aqui va la pagina de KCan HDfd </h1>
    )
 
   }
 }
 
-render(
-  <Prueba />
-  ,document.getElementById('App')
-);
+export default Prueba;
